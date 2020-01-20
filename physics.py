0dea19735a5987ad2a88b4c5e94559514e40b968
Ravid Hausmann <github@ravid.de>
2020-01-20 13:34:03 +0100

    Control animation time and simulationtime separately

--- physics.py
+++ physics.py
@@ -10,6 +10,7 @@ Created on Fri Jan 17 12:15:41 2020
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import time
 
 class Particle():
     pos = []
@@ -17,7 +18,7 @@ class Particle():
     walls = []
     def __init__(self, position, walls):
         self.pos = np.array(position, 'float64')
-        self.vel = np.array([1 for cord in self.pos], 'float64')
+        self.vel = np.array([0 for cord in self.pos], 'float64')
         self.walls = walls
         
     def update(self, dt):
@@ -26,7 +27,10 @@ class Particle():
         self.pos += self.vel*dt
         coll = self.collide()
         if coll!=None:
-            vec_m = np.multiply(np.array([1,coll], 'float64'), 1/np.sqrt(1+coll**2))
+            if coll == 'inf':
+                vec_m = np.array([0,1], 'float64')
+            else:
+                vec_m = np.multiply(np.array([1,coll], 'float64'), 1/np.sqrt(1+coll**2))
             vec_along = np.dot(vec_m, self.vel) * vec_m
             self.vel = 2 * vec_along - self.vel 
     
@@ -40,7 +44,7 @@ class Particle():
                     if self.pos[1] < y_range[0] + m * s:
                         return m
                 else: 
-                    return 1
+                    return 'inf'
         return None
                     
     
@@ -54,31 +58,36 @@ class Wall():
         
         
         
+animation_speed = 1
+animation_acc = 60 #calculations per simulated second
+fps = 30 #frames per real second
+
 
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
-ws = [Wall([[0,10],[0.2,0.2]]), Wall([[4,10],[0,3]]), Wall([[0,3],[3,0]])]
+ws = [Wall([[0,10],[0.2,0.2]]), Wall([[4,10],[0,3]]), Wall([[0,3],[3,0]]), Wall([[0,0],[0,10]])]
 for w in ws:
     x_range, y_range = w.line()
     plt.plot(x_range, y_range)
 point, = ax.plot([], [], 'ro', lw=2)
 arrow, = ax.plot([], [], 'r-', lw=2)
-p = Particle([1,2.2],ws)
+p = Particle([3.5, 3],ws)
 
-# initialization function: plot the background of each frame
+# initialization function: plot the background of each frame. Repeats every second
 def init():
     point.set_data([], [])
     return point,
 
 # animation function.  This is called sequentially
 def animate(i):
-    p.update(0.0008)
+    for k in range(int(animation_acc*animation_speed)):
+        p.update(1/animation_acc/fps)
     x, y = p.pos
     point.set_data([x], [y])
     return point,
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=2, blit=True)
+                               frames=fps, interval=int(1000/fps), blit=True)
 
 plt.plot()
